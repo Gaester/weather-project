487f43b4818c2e2c31c9f447a3ff6f19c0d80034
Melih Selvi <melihselvi229@gmail.com>
2021-05-07 00:13:26 +0300

    user interface improvements

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,7 @@ const observation_time = document.querySelector('#observation_time');
 const description = document.querySelector('#description');
 const humidity = document.querySelector('#humidity');
 const feelslike = document.querySelector('#feelslike');
+const forecastHeader = document.querySelector('#forecast-header');
 
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -24,15 +25,17 @@ weatherForm.addEventListener('submit', (e)=>{
             if(data.error){
                 return message1.textContent= data.error;
             }
+            message1.textContent="";
+            forecastHeader.textContent='Weather Forecast for '+ data.location.split(',')[0];
             forecastBox.style.display='block';
             weatherImg.setAttribute('src' , data.forecast.icon_url);
-            address.textContent= 'Location: '+data.location;
-            description.textContent='Status: '+data.forecast.description;
-            temperature.textContent= 'Temperature: '+data.forecast.temperature+'°C';
-            feelslike.textContent='Feels Like: '+data.forecast.feelslike+'°C';
-            humidity.textContent='Humidity: '+data.forecast.humidity;
-            localtime.textContent= 'Local Time: '+(data.forecast.localtime).replaceAll('-','/');
-            observation_time.textContent= 'Observation Time: '+data.forecast.observation_time;
+            address.textContent=data.location;
+            description.textContent=data.forecast.description;
+            temperature.textContent=data.forecast.temperature+'°C';
+            feelslike.textContent=data.forecast.feelslike+'°C';
+            humidity.textContent=data.forecast.humidity+ '%';
+            localtime.textContent= (data.forecast.localtime).replaceAll('-','/');
+            observation_time.textContent=data.forecast.observation_time;
 
             message1.textContent="";
         })
--- src/app.js
+++ src/app.js
@@ -76,18 +76,6 @@ app.get('/weather', (req,res)=>{
     
 })
 
-app.get('/products', (req,res)=>{
-    if(!req.query.search){
-        return res.send({
-            error:'You must provide a search term!'
-        })
-    }
-    console.log(req.query.search);
-    res.send({
-        products:[]
-    })
-})
-
 app.get('/help/*', (req,res)=>{
     res.render('error',{
         message:'Help article not found.',
